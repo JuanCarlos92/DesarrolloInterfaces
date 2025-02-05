0cf39952e9d65f2e90fbb3b1813cd12135983f79
Juan Carlos <juancarlosfilter@gmail.com>
2025-02-05 16:49:29 +0100

    restaurante crud

---
+++ CRUDRestaurante/src/main/java/org/example/crudrestaurante/models/Producto.java
@@ -0,0 +1,57 @@
+package org.example.crudrestaurante.models;
+
+import java.math.BigDecimal;
+
+public class Producto {
+
+    private Integer id;
+
+    private String nombre;
+
+    private String categoria;
+
+    private BigDecimal precio;
+
+    private Boolean disponibilidad = false;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public BigDecimal getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
+
+    public Boolean getDisponibilidad() {
+        return disponibilidad;
+    }
+
+    public void setDisponibilidad(Boolean disponibilidad) {
+        this.disponibilidad = disponibilidad;
+    }
+
+}
